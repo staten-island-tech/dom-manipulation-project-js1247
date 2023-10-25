ba72b100552ec5b6e2890e0a688690137dd68236
js1247 <jasonshi1247@gmail.com>
2023-10-25 09:24:08 -0400

    dom

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+const DOMSelectors = {
+    input: document.querySelector("#input"),
+    block: document.querySelector(".block"),
+    color: document.querySelector(".block"),
+    button: document.getElementById("click me"),
+    text: document.querySelectorAll("#select"),
+    input: document.querySelector(`#input`),
+}
+
+function add() {
+    DOMSelectors.button.insertAdjacentElement("#input")
+    {DOMSelectors.block.onClick} {DOMSelectors.color.onClick}
+} 
+
+function remove() {
+    DOMSelectors.button
+}
+
+document.body.style.backgroundColor = "purple";
+
+console.log (DOMSelectors);
