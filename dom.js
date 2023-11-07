b61ce2a60a0da4c56c27ff0832b944108ec83996
js1247 <jasonshi1247@gmail.com>
2023-11-07 15:00:51 -0500

    js

--- dom.js
+++ dom.js
@@ -1,28 +1,42 @@
 const DOMSelectors = {
-    form: document.getElementById("form"),
-    name: document.getElementById("name"),
-    country: document.getElementById("country"),
-    image: document.getElementById("image"),
-    button: document.getElementById("btn"),
-}
-
-DOMSelectors.button.addEventListener("sumbit", function() {
-
-})
-
-function display() {
-
+  form: document.getElementById("form"),
+  name: document.getElementById("name"),
+  country: document.getElementById("country"),
+  image: document.getElementById("image"),
+  button: document.getElementById("btn"),
+  cards: document.getElementById("display"),
+};
+function add() {
+  DOMSelectors.cards.insertAdjacentHTML(
+    "beforeend",
+    `<div class="cards">
+        <h2>${DOMSelectors.name.value}</h2>
+        <h3>${DOMSelectors.country.value}</h3>
+        <div>
+        <img class="card-img" src= "${DOMSelectors.image.value}" alt="image">
+        </div>
+        <button class ="remove">Remove</button>
+    </div>`
+  );
 }
 
 function remove() {
-    
+  document.querySelectorAll(".remove").forEach((button) => {
+    button.addEventListener("click", function (button) {
+      button.target.parentElement.remove();
+    });
+  });
 }
 
-function clear() {
-    
+function clearInputs() {
+  DOMSelectors.name.value = "";
+  DOMSelectors.country.value = "";
+  DOMSelectors.image.value = "";
 }
 
-DOMSelectors.form.addEventListener("submit", function () {
-
-})
-
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  add();
+  remove();
+  clearInputs();
+});
